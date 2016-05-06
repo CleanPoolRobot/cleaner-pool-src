689e164c0b840e4ec9f32aea74d2538052b082a7
TomazMartins <tomaz.r.martins@gmail.com>
2016-05-06 14:09:20 +0000

    Changing pinout
    Signed-off-by: Tomaz Martins <tomaz.r.martins@gmail.com>
    Signed-off-by: Maxwell Almeida <llewxam150@gmail.com>

--- src/constants/pinout.hpp
+++ src/constants/pinout.hpp
@@ -8,25 +8,21 @@ namespace
   const int POWER_SUPPLY_1 = RPI_V2_GPIO_P1_02;
   const int POWER_SUPPLY_2 = RPI_V2_GPIO_P1_04;
 
-  const int MUX_PRESSURE = RPI_V2_GPIO_P1_07;
-  const int MUX_ENGINE = RPI_V2_GPIO_P1_40;
+  const int MUX_ENGINE = RPI_V2_GPIO_P1_11;
 
-  const int PRESSURE_B1 = RPI_V2_GPIO_P1_38;
-  const int PRESSURE_B2 = RPI_V2_GPIO_P1_37;
-  const int PRESSURE_B3 = RPI_V2_GPIO_P1_36;
-  const int PRESSURE_B4 = RPI_V2_GPIO_P1_35;
-  const int PRESSURE_B5 = RPI_V2_GPIO_P1_33;
-  const int PRESSURE_B6 = RPI_V2_GPIO_P1_32;
-  const int PRESSURE_B7 = RPI_V2_GPIO_P1_31;
-  const int PRESSURE_B8 = RPI_V2_GPIO_P1_29;
-  
-  const int BRIDGE_H1_ENGINE = RPI_V2_GPIO_P1_11;
-  const int BRIDGE_H2_ENGINE = RPI_V2_GPIO_P1_12;
-
-  const int ENGINE_CONTROL = RPI_V2_GPIO_P1_13;
+  const int BRIDGE_H0_ENGINE = RPI_V2_GPIO_P1_13;
+  const int BRIDGE_H1_ENGINE = RPI_V2_GPIO_P1_15;
 
   const int AG_SDA1_I2C = RPI_V2_GPIO_P1_03;
   const int AG_SCL1_I2C = RPI_V2_GPIO_P1_05;
+
+  const int ARDUINO_TX = RPIO_V2_GPIO_P1_08;
+  const int ARDUINO_RX = RPIO_V2_GPIO_P1_10;
+
+  const int FRONT_GATE = RPIO_V2_GPIO_P1_35;
+  const int BACK_GATE = RPIO_V2_GPIO_P1_36;
+  const int LEFT_GATE = RPIO_V2_GPIO_P1_37;
+  const int RIGHT_GATE = RPIO_V2_GPIO_P1_38;
 }
 
 #endif // PINOUT_HPP
