f1a1edec8de0b044ff9c4393277b5ae7f459c1e9
Thaiane <thaianefbraga@gmail.com>
2016-05-06 20:23:16 +0000

    Communication testing
    Signed-off-by: Thaiane <thaianefbraga@gmail.com>
    Signed-off-by: TomazMartins <tomaz.r.martins@gmail.com>
    Signed-off-by: MaxAlmeida <llewxam150@gmail.com>

--- src/service/communication.cpp
+++ src/service/communication.cpp
@@ -1,18 +1,20 @@
 #include "communication.hpp"
 #include <termios.h>
 #include <unistd.h>
-
+#include <iostream>
+using namespace std;
 namespace IO
 {
   const int SIZE_BUFFER = 50;
 
   int start_arduino()
   {
-    int id_device = open( DEVICE, O_RONLY );
+    int id_device = open( DEVICE, O_RDWR | O_CREAT );
 
     if( id_device != NOT_OPEN )
     {
-      struct termios options;
+      std::cout << "Abri o arquivo " << std::endl;
+      /*struct termios options;
 
       tcgetattr( id_device, &options );
 
@@ -22,7 +24,7 @@ namespace IO
       options.c_lflag = 0;
 
       tcflush( id_device, TCIFLUSH );
-      tcsetattr( id_device, TCSANOW, &options );
+      tcsetattr( id_device, TCSANOW, &options );*/
     } else
     {
       std::cout << "Unable to open Arduino communication port..." << std::endl;
@@ -35,10 +37,13 @@ namespace IO
   {
     char buffer[ SIZE_BUFFER ];
     int check = read( id_device, (void *)buffer, sizeof( buffer ) );
-
+  
     if( check != NOT_OPEN )
     {
+      std::cout << "Estou lendo aqui o arquivo .. " << std::endl;
       // Nothing to do
+      if(buffer[0] == 'A')
+        std::cout << "EHHH PEGUEI UM A NO ARQUIVO" << std::endl;
     } else
     {
       std::cout << "Unable to read arduino..." << std::endl;
@@ -50,6 +55,7 @@ namespace IO
   int write_arduino( int id_device )
   {
     char buffer[ SIZE_BUFFER ];
+    buffer[0] = 'A';
     int check = write( id_device, (void *)buffer, sizeof( buffer ) );
 
     if( check != NOT_OPEN )
