56e8d6f329f78f345a5889bddd640658d17f20b0
TomazMartins <tomaz.r.martins@gmail.com>
2016-05-06 15:31:46 +0000

    Implementing communication.hpp and function start_arduino()
    Signed-off-by: Tomaz Martins <tomaz.r.martins@gmail.com>
    Signed-off-by: Maxwell Almeida <llewxam150@gmail.com>

---
+++ src/service/communication.hpp
@@ -0,0 +1,28 @@
+#ifndef COMMUNICATION_HPP
+#define COMMUNICATION_HPP
+
+#include <fcntl.h> // Define some flags to open function.
+
+#define DEVICE "/dev/ttyAMA0"
+#define NOT_OPEN -1
+
+namespace IO
+{
+  public:
+    int start_arduino();
+    int read_arduino( int );
+    int write_arduino( int );
+    int stop_arduino( int );
+
+    int read_ag();
+    int write_ag();
+
+    int read_water_pump();
+    int write_water_pump();
+
+    int open_gate();
+
+    int active_engine();
+}
+
+#endif COMMUNICATION_HPP
