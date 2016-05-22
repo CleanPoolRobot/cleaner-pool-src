44df35b94bcc852386678e95f2ee787c939e15e8
MaxAlmeida <llewxam150@gmail.com>
2016-05-22 14:43:12 -0300

    Método move_right()
    Signed-off-by: Tomaz Martins <tomaz.r.martins@gmail.com>
    Signed-off-by: Thaiane Braga <thaianefbraga@gmail.com>
    Signed-off-by: Maxwell Almeida <llewxam150@gmail.com>

--- src/app.py
+++ src/app.py
@@ -20,17 +20,53 @@ def activate_water_pump():
     return
 
 
+def deactivate_water_pump():
+    return
+
+
+def turn_pipe(angle):
+    return
+
+
+def turn_wheels(angle):
+    return
+
+
+def activate_front_distance_sensor():
+    return
+
+
+def deactivate_brush():
+    return
+
+
 def move_front():
-    turn_pipe(0)
-    turn_wheel(0)
+    deactivate_water_pump()
+    turn_pipe(0)  # degree
+    turn_wheels(0)  # degree
 
     activate_water_pump()
+
     while FOREVER:
-        distance = activate_front_distance_sensor()  # cm
+        distance = activate_front_distance_sensor()  #  cm
 
         if distance <= 5:
             deactivate_water_pump()
+            deactivate_brush()
             break
+
+    return
+
+
+def move_right():
+    deactivate_water_pump()
+    turn_pipe(90)
+    turn_wheels(90)
+
+    activate_water_pump()
+    time.sleep(5)  # second
+
+    deactivate_water_pump
 
     return
 
