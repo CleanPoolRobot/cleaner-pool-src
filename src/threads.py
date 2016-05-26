9092c5223952eda719fc6c34191f0563c7933638
MaxAlmeida <llewxam150@gmail.com>
2016-05-26 00:52:17 -0300

    Adicionando semaforos para uso de recursos compartilhados

--- src/threads.py
+++ src/threads.py
@@ -1,14 +1,22 @@
 import thread
 import communication as com
 
+semaphore = thread.allocate_lock()
+
 def request_distance(distance, arduino_communication, request_type)
-  com.write_arduino(arduino_communication,request_type)
-  distance = com.read_arduino(arduino_communication)
+  while True:
+    semaphore.acquire()
+    com.write_arduino(arduino_communication,request_type)
+    distance = com.read_arduino(arduino_communication)
+    semaphore.release()
   return distance
 
 def request_pressure(pressure, arduino_communication, request_type)
-  com.write_arduino(arduino_communication,request_type)
-  pressure = com.read_arduino(arduino_communication)
+  while True:
+    semaphore.acquire()
+    com.write_arduino(arduino_communication,request_type)
+    pressure = com.read_arduino(arduino_communication)
+    semaphore.release()
   return pressure
          
   
